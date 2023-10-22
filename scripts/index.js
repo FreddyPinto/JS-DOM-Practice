a481f7fb954d2342144bcf1a35c2ff3302cb4dc3
FreddyPinto <freddypinto@outlook.com>
2023-10-22 18:28:51 -0500

    Add import digitalClock function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./menu_hamburguer.js";
+import { digitalClock, alarm } from "./clock.js";
 
 const d = document
 
 d.addEventListener("DOMContentLoaded", (e) => {
 hamburgerMenu(".panel-btn", ".panel", ".menu a");
+digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
 })
